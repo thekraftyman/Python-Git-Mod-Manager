4548d4f6c2127b229f37943b0bf7b3c1092912c4
thekraftyman <thekraftyman@gmail.com>
2021-01-25 20:45:25 -0600

    more fun changes

--- core/misc.py
+++ core/misc.py
@@ -10,10 +10,10 @@ import json
 def load_json(filename):
     # open and read the existing json
     with open(filename, 'r') as infile:
-        json_dic = json.loads(infile.read())
+        json_dic = json.load(infile)
     return json_dic
 
 def save_json(filename, dictionary):
     # save dictionary to jsons
-    with open(filename, 'w') as outfile:
-        outfile.write(json.dumps(dictionary))
+    with open(filename, 'w+') as outfile:
+        json.dump(dictionary,outfile)
--- main.py
+++ main.py
@@ -9,9 +9,23 @@ from shutil import copyfile, rmtree
 
 # Fun Globals ---------
 DIR_DELIM = '/' if platform.system() != 'Windows' else '\\'
-CORE_GAME_FILES = 'var' + DIR_DELIM + 'core_files.json'
+BASE_DIR = os.getcwd()
+CORE_GAME_FILES = BASE_DIR + DIR_DELIM + 'var' + DIR_DELIM + 'core_files.json'
+USER_VARS = BASE_DIR + DIR_DELIM + 'var' + DIR_DELIM + 'user_vars.json'
 
 def main():
+    # initialize with user vars
+    if not os.path.exists(USER_VARS):
+        user_vars = {}
+        user_vars['base_game_path'] = input('Paste full path to base Among Us directory: ')
+        while not os.path.isdir(user_vars['base_game_path']):
+            print(user_vars['base_game_path'])
+            user_vars['base_game_path'] = input('Incorrect path given, please input full path: ')
+        # write to json
+        save_json(USER_VARS, user_vars)
+    else:
+        user_vars = load_json(USER_VARS)
+
     # get core files
     game_files = load_json(CORE_GAME_FILES)
 
